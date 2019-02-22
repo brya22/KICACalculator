589eeb4a71990acc2ef925052768289919b8d381
Bryan <pacher@sheridancollege.ca>
2019-02-22 01:18:02 -0500

    Initial Commit

---
+++ KICA CAL V2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  KICA CAL V2
+//
+//  Created by Bryan Pacher on 2019-02-22.
+//  Copyright © 2019 Bryan Pacher. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KICA CAL V2Tests/KICA_CAL_V2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  KICA_CAL_V2Tests.swift
+//  KICA CAL V2Tests
+//
+//  Created by Bryan Pacher on 2019-02-22.
+//  Copyright © 2019 Bryan Pacher. All rights reserved.
+//
+
+import XCTest
+@testable import KICA_CAL_V2
+
+class KICA_CAL_V2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
